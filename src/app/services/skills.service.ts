385674c449628ec593c7fabac35ae739154518fe
Fred <fredsg22@hotmail.com>
2025-07-28 00:44:11 -0600

    Test

--- src/app/services/skills.service.ts
+++ src/app/services/skills.service.ts
@@ -9,7 +9,7 @@ export class SkillsService {
   constructor(private httpClient: HttpClient) { }
 
   // private baseUrl = 'http://localhost:3000';
-  private baseUrl = 'https://quick-programming-tests-backend.onrender.com/'
+  private baseUrl = 'https://quick-programming-tests-backend.onrender.com'
 
   getAllSkills(): Observable<any[]> {
     return this.httpClient.get<any[]>(`${this.baseUrl}/api/skills`);
